b0cdf3253fecda48044d2cd4ed5032bfff73da67
ssampathkumar <ssampathkumar@uco.edu>
2024-01-25 13:54:32 -0500

    initial commit

---
+++ src/main/java/com/example/SpringReceiptProcessor/SpringReceiptProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringReceiptProcessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReceiptProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReceiptProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringReceiptProcessor/SpringReceiptProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringReceiptProcessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReceiptProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
