a4fcd4ec8724f43a0dcd3f8bc3091ed875275873
ssampathkumar <ssampathkumar@uco.edu>
2024-01-25 15:44:53 -0500

    hello-api

--- src/main/java/com/example/SpringReceiptProcessor/SpringReceiptProcessorApplication.java
+++ src/main/java/com/example/SpringReceiptProcessor/SpringReceiptProcessorApplication.java
@@ -3,7 +3,9 @@ package com.example.SpringReceiptProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class SpringReceiptProcessorApplication {
 
 	public static void main(String[] args) {
